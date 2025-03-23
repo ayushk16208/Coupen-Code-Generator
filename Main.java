8af47e21e7fd64cb0de487d9e642b462f20c270c
ayushk16208 <135859649+ayushk16208@users.noreply.github.com>
2025-03-23 19:06:23 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ import controller.CouponController;
 
 public class Main {
     public static void main(String[] args) {
-        // Step 1: Create instances of CouponModel and CouponView
+        
         CouponModel couponModel = new CouponModel();
         CouponView couponView = new CouponView();
 
-        // Step 2: Create an instance of CouponController and pass the model and view
+       
         CouponController couponController = new CouponController(couponModel, couponView);
 
-        // Step 3: Call the method to generate the coupon for the user
+
         couponController.generateCouponForUser();
     }
 }
